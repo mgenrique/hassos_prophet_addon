16440c2fd5a17d2d40a846332c8aeca3d3e559da
Enrique <enriquemg.75@gmail.com>
2024-11-08 00:39:54 +0100

    Update run.sh

--- prophet-influx-multi-addon/run.sh
+++ prophet-influx-multi-addon/run.sh
@@ -3,15 +3,6 @@
 # Mensaje de inicio
 echo "Iniciando el addon Prophet InfluxDB..."
 
-# Lee las configuraciones del archivo /data/options.json y las exporta como variables de entorno
-
-export INFLUXDB_HOST=$(jq -r '.influxdb_host' /data/options.json)
-export INFLUXDB_PORT=$(jq -r '.influxdb_port' /data/options.json)
-export INFLUXDB_USER=$(jq -r '.influxdb_user' /data/options.json)
-export INFLUXDB_PASSWORD=$(jq -r '.influxdb_password' /data/options.json)
-export INFLUXDB_DBNAME=$(jq -r '.influxdb_dbname' /data/options.json)
-export PORT=$(jq -r '.port' /data/options.json)
-
 # Ejecuta la aplicación principal
 exec python /app/main.py
 
